01a066f497e34dec6e30e9a69687e8bf8e0cb782
tomasort <tomasvortegar@gmail.com>
2024-06-14 21:09:25 +0000

    got a simple socket app working

--- public/Assets/js/app.js
+++ public/Assets/js/app.js
@@ -1,7 +1,29 @@
 var myapp = (function(){
-    function init(userId, meetingId){
-        console.log('User ID: ', userId);
-        console.log('Meeting ID: ', meetingId);
+    var socket=null;
+    var userId = null;
+    var meetingId = null;
+
+    function init(uid, mid){
+        userId = uid;
+        meetingId = mid;
+        console.log('User ID: ', uid);
+        console.log('Meeting ID: ', mid);
+        event_process_for_signaling_server();
+    }
+
+    function event_process_for_signaling_server() {
+        socket = io.connect("http://192.168.1.2:3000");
+        socket.on("connect", function(){
+            console.log("Connected to signaling server");
+            if (socket.connected) {
+                if (userId && meetingId) {
+                    socket.emit("user_connect", {
+                        userId: userId,
+                        meetingId: meetingId
+                    });
+                }
+            }
+        });
     }
     return {
         _init: function(userId, meetingId){
