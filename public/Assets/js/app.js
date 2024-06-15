0e96fddbd5fb747c98f8f83cea6293267906af98
tomasort <tomasvortegar@gmail.com>
2024-06-15 06:51:17 +0000

    working on webrtc now

--- public/Assets/js/app.js
+++ public/Assets/js/app.js
@@ -1,9 +1,46 @@
-var myapp = (function(){
-    var socket=null;
+var peerConnectionManager = (function () {
+    function _init(SDP_function, myConnId) {
+        console.log("Init App Process");
+
+    }
+    function setNewConn(connId) {
+        console.log("New Connection: ", connId);
+        var connection = new RTCPeerConnection({
+            iceServers: [
+                {
+                    urls: "stun:stun.l.google.com:19302"
+                },
+                {
+                    urls: "stun:stun1.l.google.com:19302"
+                }
+            ]
+        });
+        connection.onnegotiationneeded = async () => {
+            await setOffer(connId)
+        }
+        connection.onicecandidate = (event) => {
+            if (event.candidate) {
+                serverProcess(JSON.stringify({ icecandidate: event.candidate }), connId);
+            }
+        }
+    }
+
+    return {
+        setNewConn: async function (connId) {
+            await setNewConn(connId);
+        },
+        init: async function (SDP_function, myConnId) {
+            await _init(SDP_function, myConnId);
+        }
+    };
+})();
+
+var rtcClient = (function () {
+    var socket = null;
     var userId = null;
     var meetingId = null;
 
-    function init(uid, mid){
+    function init(uid, mid) {
         userId = uid;
         meetingId = mid;
         console.log('User ID: ', uid);
@@ -11,11 +48,30 @@ var myapp = (function(){
         event_process_for_signaling_server();
     }
 
+    function add_new_user_connection(other_user_id, connId) {
+        var newDivid = $("#otherTemplate").clone();
+        newDivid = newDivid.attr("id", connId).addClass("other");
+        newDivid.find("h2").text(other_user_id);
+        newDivid.find("video").attr("id", "v_" + connId);
+        newDivid.find("audio").attr("id", "a_" + connId);
+        newDivid.show();
+        $("#divUsers").append(newDivid);
+
+    }
+
+
     function event_process_for_signaling_server() {
         socket = io.connect("http://192.168.1.2:3000");
-        socket.on("connect", function(){
+        var SDP_function = function (data, connId) {
+            socket.emit("SDP_process", {
+                messge: data,
+                connId: connId
+            });
+        }
+        socket.on("connect", function () {
             console.log("Connected to signaling server");
             if (socket.connected) {
+                peerConnectionManager.init(SDP_function, socket.id);
                 if (userId && meetingId) {
                     socket.emit("user_connect", {
                         userId: userId,
@@ -24,9 +80,16 @@ var myapp = (function(){
                 }
             }
         });
+        // listen for new participant
+        socket.on("new_participant", function (data) {
+            console.log("New participant added ", data);
+            add_new_user_connection(data.other_user_id, data.connId);
+            peerConnectionManager.setNewConn(data.connId);
+        });
     }
+
     return {
-        _init: function(userId, meetingId){
+        _init: function (userId, meetingId) {
             init(userId, meetingId);
         }
     }
--- server.js
+++ server.js
@@ -9,10 +9,26 @@ var server = app.listen(3000, function() {
 const io = require("socket.io")(server);
 app.use(express.static(path.join(__dirname, "")));
 
+// TODO: refactor this code to use a database to store user connections
+var userConnections = [];
 
 io.on("connection", (socket) => {
     socket.on("user_connect", (data) => {
-        console.log("User connected: ", data.userId);
-        console.log("Meeting ID: ", data.meetingId);
+        console.log("User connected: ", data.userId, " Meeting ID: ", data.meetingId);
+        // get the users with the same meeting ID
+        var other_users = userConnections.filter((p) => p.meetingId == data.meetingId);
+        userConnections.push({
+            socketId: socket.id,
+            userId: data.userId,
+            meetingId: data.meetingId
+        });
+        // send the other users in the conference the new user's connection details
+        other_users.forEach((v) => {
+            console.log("Emitting to: ", v.userId);
+            socket.to(v.socketId).emit("new_participant", {
+                other_user_id: data.userId,
+                connId: socket.id
+            });
+        });
     });
 });
